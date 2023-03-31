6a49c99df094608a961bd2bd0eacb1c64d0dc92c
euwest2 <129495341+euwest2@users.noreply.github.com>
2023-03-31 09:38:25 +0100

    Added streamlit text input

--- loan-calculator.py
+++ loan-calculator.py
@@ -1,3 +1,4 @@
+import streamlit as st
 import math
 
 print("What do you want to calculate?")
@@ -5,11 +6,11 @@ print('type "n" for number of monthly payments,')
 print('type "a" for annuity monthly payment amount,')
 print('type "p" for loan principal:')
 
-option = input("> ")
+option = st.text_input("> ")
 if option == "n":
-    loan_prin = float(input("Enter the loan principal: "))
-    mon_pay = float(input("Enter the monthly payment: "))
-    loan_int = float(input("Enter the loan interest: ")) / 1200
+    loan_prin = float(st.text_input("Enter the loan principal: "))
+    mon_pay = float(st.text_input("Enter the monthly payment: "))
+    loan_int = float(st.text_input("Enter the loan interest: ")) / 1200
     num_periods = math.ceil(math.log((mon_pay / (mon_pay - (loan_int * loan_prin))), (1 + loan_int)))
     if num_periods == 1:
         print("It will take", num_periods, "month to repay this loan")
@@ -21,15 +22,15 @@ if option == "n":
         print("It will take", num_periods_years, "years and", num_periods_months, "months to repay this loan!")
 
 if option == "a":
-    loan_prin = float(input("Enter the loan principal: "))
-    num_periods = float(input("Enter the number of periods: "))
-    loan_int = float(input("Enter the loan interest: ")) / 1200
+    loan_prin = float(st.text_input("Enter the loan principal: "))
+    num_periods = float(st.text_input("Enter the number of periods: "))
+    loan_int = float(st.text_input("Enter the loan interest: ")) / 1200
     mon_pay = round((loan_prin * ((loan_int * pow(1 + loan_int, num_periods))/(pow(1 + loan_int, num_periods) - 1))),2)
     print("Your monthly payment = $", mon_pay)
 
 if option == "p":
-    mon_pay = float(input("Enter the annuity payment: "))
-    num_periods = float(input("Enter the number of periods: "))
-    loan_int = float(input("Enter the loan interest: ")) / 1200
+    mon_pay = float(st.text_input("Enter the annuity payment: "))
+    num_periods = float(st.text_input("Enter the number of periods: "))
+    loan_int = float(st.text_input("Enter the loan interest: ")) / 1200
     loan_prin = round((mon_pay / ((loan_int * pow(1 + loan_int, num_periods))/(pow(1 + loan_int, num_periods) - 1))),2)
     print("Your loan principal = $", loan_prin)
