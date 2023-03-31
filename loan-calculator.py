6a2470531346fc7984f6399ecede86bf6a7b26de
euwest2 <129495341+euwest2@users.noreply.github.com>
2023-03-31 09:19:03 +0100

    uploaded loan-calculator.py

---
+++ loan-calculator.py
@@ -0,0 +1,35 @@
+import math
+
+print("What do you want to calculate?")
+print('type "n" for number of monthly payments,')
+print('type "a" for annuity monthly payment amount,')
+print('type "p" for loan principal:')
+
+option = input("> ")
+if option == "n":
+    loan_prin = float(input("Enter the loan principal: "))
+    mon_pay = float(input("Enter the monthly payment: "))
+    loan_int = float(input("Enter the loan interest: ")) / 1200
+    num_periods = math.ceil(math.log((mon_pay / (mon_pay - (loan_int * loan_prin))), (1 + loan_int)))
+    if num_periods == 1:
+        print("It will take", num_periods, "month to repay this loan")
+    if num_periods < 12:
+        print("It will take", num_periods, "months to repay this loan")
+    if num_periods > 12:
+        num_periods_years = math.floor(num_periods / 12)
+        num_periods_months = num_periods - (num_periods_years * 12)
+        print("It will take", num_periods_years, "years and", num_periods_months, "months to repay this loan!")
+
+if option == "a":
+    loan_prin = float(input("Enter the loan principal: "))
+    num_periods = float(input("Enter the number of periods: "))
+    loan_int = float(input("Enter the loan interest: ")) / 1200
+    mon_pay = round((loan_prin * ((loan_int * pow(1 + loan_int, num_periods))/(pow(1 + loan_int, num_periods) - 1))),2)
+    print("Your monthly payment = $", mon_pay)
+
+if option == "p":
+    mon_pay = float(input("Enter the annuity payment: "))
+    num_periods = float(input("Enter the number of periods: "))
+    loan_int = float(input("Enter the loan interest: ")) / 1200
+    loan_prin = round((mon_pay / ((loan_int * pow(1 + loan_int, num_periods))/(pow(1 + loan_int, num_periods) - 1))),2)
+    print("Your loan principal = $", loan_prin)
