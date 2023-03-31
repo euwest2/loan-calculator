e30031cc5692c67361bdfbc2efbdf1cb3b33215e
euwest2 <129495341+euwest2@users.noreply.github.com>
2023-03-31 10:22:20 +0100

    Replaced print with st.write

--- loan-calculator.py
+++ loan-calculator.py
@@ -1,10 +1,10 @@
 import streamlit as st
 import math
 
-print("What do you want to calculate?")
-print('type "n" for number of monthly payments,')
-print('type "a" for annuity monthly payment amount,')
-print('type "p" for loan principal:')
+st.write("What do you want to calculate?")
+st.write('type "n" for number of monthly payments,')
+st.write('type "a" for annuity monthly payment amount,')
+st.write('type "p" for loan principal:')
 
 option = st.text_input("> ")
 if option == "n":
@@ -13,24 +13,24 @@ if option == "n":
     loan_int = float(st.number_input("Enter the loan interest: ")) / 1200
     num_periods = math.ceil(math.log((mon_pay / (mon_pay - (loan_int * loan_prin))), (1 + loan_int)))
     if num_periods == 1:
-        print("It will take", num_periods, "month to repay this loan")
+        st.write("It will take", num_periods, "month to repay this loan")
     if num_periods < 12:
-        print("It will take", num_periods, "months to repay this loan")
+        st.write("It will take", num_periods, "months to repay this loan")
     if num_periods > 12:
         num_periods_years = math.floor(num_periods / 12)
         num_periods_months = num_periods - (num_periods_years * 12)
-        print("It will take", num_periods_years, "years and", num_periods_months, "months to repay this loan!")
+        st.write("It will take", num_periods_years, "years and", num_periods_months, "months to repay this loan!")
 
 if option == "a":
     loan_prin = float(st.number_input("Enter the loan principal: "))
     num_periods = float(st.number_input("Enter the number of periods: "))
     loan_int = float(st.number_input("Enter the loan interest: ")) / 1200
     mon_pay = round((loan_prin * ((loan_int * pow(1 + loan_int, num_periods))/(pow(1 + loan_int, num_periods) - 1))),2)
-    print("Your monthly payment = $", mon_pay)
+    st.write("Your monthly payment = $", mon_pay)
 
 if option == "p":
     mon_pay = float(st.number_input("Enter the annuity payment: "))
     num_periods = float(st.number_input("Enter the number of periods: "))
     loan_int = float(st.number_input("Enter the loan interest: ")) / 1200
     loan_prin = round((mon_pay / ((loan_int * pow(1 + loan_int, num_periods))/(pow(1 + loan_int, num_periods) - 1))),2)
-    print("Your loan principal = $", loan_prin)
+    st.write("Your loan principal = $", loan_prin)
